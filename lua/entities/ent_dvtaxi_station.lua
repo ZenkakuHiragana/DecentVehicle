6b68c8afa5b91d0459b8d59c0846d005bc3b9208
Zenkaku Hiragana <daiko1997@yahoo.co.jp>
2025-01-11 21:56:20 +0900

    ✨️ feat: Improved taxi station: generating random name on spawn and showing the name on the model

--- lua/entities/ent_dvtaxi_station.lua
+++ lua/entities/ent_dvtaxi_station.lua
@@ -22,13 +22,94 @@ function ENT:SetupDataTables()
 end
 
 if CLIENT then
+    ENT.TextWidth = 0
+    ENT.TextPosition = 75
+    ENT.LastTimeScrollChanged = CurTime()
+    local CAM_SCALE = 1 / 8
+    local MAX_WIDTH = 24 / CAM_SCALE
+    local SCROLL_PIXELS_PER_SECOND = 75
+    local MAX_DISTANCE = 2000^2
     function ENT:Draw()
         self:DrawModel()
+        if self:GetForward():Dot(EyePos() - self:WorldSpaceCenter()) > 0 then return end
+        if EyePos():DistToSqr(self:WorldSpaceCenter()) > MAX_DISTANCE then return end
+
+        -- Set up text position
+        local origin = self:LocalToWorld(Vector(-1.0, 12.0, 36.5))
+        local cliporigin = origin + self:GetRight() * 24
+        local angles = self:GetAngles()
+        local x = math.Clamp(self.TextPosition, MAX_WIDTH - self.TextWidth, 0)
+        angles:RotateAroundAxis(self:GetUp(), -90)
+        angles:RotateAroundAxis(self:GetRight(), 90)
+
+        -- Make sure to draw the text on the model
+        render.PushCustomClipPlane(self:GetRight(), self:GetRight():Dot(origin))
+        render.PushCustomClipPlane(-self:GetRight(), -self:GetRight():Dot(cliporigin))
+
+        -- Draw station name
+        cam.Start3D2D(origin, angles, CAM_SCALE)
+        local width, _ = draw.SimpleText(self:GetStationName(), "DermaLarge", x, 0)
+        cam.End3D2D()
+
+        -- Back to normal state
+        render.PopCustomClipPlane()
+        render.PopCustomClipPlane()
+
+        -- Scroll the text if it's too long
+        self.TextWidth = width
+        self.TextPosition = self.TextPosition - RealFrameTime() * SCROLL_PIXELS_PER_SECOND
+
+        -- Reset scroll position if the name fits in the model or it finished scrolling
+        if width < MAX_WIDTH or self.TextPosition < MAX_WIDTH - width - SCROLL_PIXELS_PER_SECOND then
+            self.TextPosition = SCROLL_PIXELS_PER_SECOND
+        end
     end
 
     return
 end
 
+-- creikey/top-1000-nouns.txt - GitHub Gist
+local NounsURL = "https://gist.githubusercontent.com/creikey/"
+              .. "42d23d1eec6d764e8a1d9fe7e56915c6/raw/"
+              .. "b07de0068850166378bc3b008f9b655ef169d354/top-1000-nouns.txt"
+-- hugsy/english-adjectives.txt - GitHub Gist
+local AdjectivesURL = "https://gist.githubusercontent.com/hugsy/"
+              .. "8910dc78d208e40de42deb29e62df913/raw/"
+              .. "eec99c5597a73f6a9240cab26965a8609fa0f6ea/english-adjectives.txt"
+local nouns = {
+    "party", "information", "school", "fact", "money", "point", "example",
+    "state", "business", "night", "area", "water", "thing", "family", "head",
+    "hand", "order", "john", "side", "home", "development", "week", "power",
+    "country", "council", "use", "service", "room", "market", "problem",
+}
+-- List of adjectives - PaperRater
+-- https://www.paperrater.com/page/lists-of-adjectives
+local adjectives = {
+    "attractive", "bald", "beautiful", "chubby", "clean", "dazzling",
+    "drab", "elegant", "fancy", "fit", "flabby", "glamorous", "gorgeous",
+    "handsome", "long", "magnificent", "muscular", "plain", "plump",
+    "quaint", "shapely", "short", "skinny", "stocky", "ugly",
+
+    "ashy", "black", "blue", "gray", "green", "icy", "lemon", "mango",
+    "orange", "purple", "red", "salmon", "white", "yellow",
+
+    "alive", "better", "careful", "clever", "easy", "famous", "gifted",
+    "hallowed", "helpful", "important", "inexpensive", "mealy",
+    "mushy", "powerful", "shy", "tender", "vast",
+}
+-- Fetch more words if available
+http.Fetch(NounsURL, function(body, _, _, _)
+    nouns = body:Trim():Split "\n"
+end)
+http.Fetch(AdjectivesURL, function(body, _, _, _)
+    adjectives = body:Trim():Split "\n"
+end)
+
+local function GetRandomName()
+    return string.format("%s-%s",
+        adjectives[math.random(#adjectives)],
+        nouns[math.random(#nouns)])
+end
 
 function ENT:Initialize()
     self:SetModel "models/decentvehicle/ent_dvtaxi_station.mdl"
@@ -61,6 +142,7 @@ function ENT:SpawnFunction(ply, tr, ClassName)
     ent:SetAngles(Angle(0, ang.yaw, 0))
     ent:SetPos(pos)
     ent:Spawn()
+    ent:SetStationName(GetRandomName())
 
     return ent
 end
